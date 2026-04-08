12ad0e8d4e76dbc3301aea3ca5d125e5a19b1b4d
Ahsan <thefirstblood29@gmail.com>
2026-04-09 03:28:45 +0500

    u1

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const API_BASE = "https://tinyurl1.up.railway.app/";
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
     </Routes>
   </BrowserRouter>,
-)
+);
